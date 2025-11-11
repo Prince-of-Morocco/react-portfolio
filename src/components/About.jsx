de37d4d2363e5bce97936548dde5e30ffcbc5ec1
Bharath Raj 1024 <94532869+Prince-of-Morocco@users.noreply.github.com>
2025-11-11 22:18:59 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,65 +3,48 @@ import React from 'react'
 function About() {
   return (
     <div
-    name="About"
-    className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20">
-    <div>
-      <h1 className="text-3xl font-bold mb-5 text-center">About</h1>
-      <p>
-      </p> 
-      <br />
-      <h1 className="text-green-600 font-semibold text-xl">
-        Education & Training
-      </h1>
-      <span>
-      MCA, Anurag University, 2023 - 2025 <br />
-      BSc Computer Science & Cognitive Systems, Loyola Academy, 2020 - 2023 <br />
-      12th, Bhavan's Sri Aurobindo Junior College, 2018 <br />
-      SSC, Pallavi Progressive High School, 2018 <br />
-      </span>
-      <br />
-      
-      <h1 className="text-green-600 font-semibold text-xl">
-        Skills & Expertise
-      </h1>
-      <span>
-      Proficient in: Java, Linux, Amazon EC2
-      </span>
-      <br />
-      <br />
-      {/*   <h1 className="text-green-600 font-semibold text-xl">
-        Professional Experience
-      </h1>
-      <span>
-        [Job Title], [Company/Organization], [Dates] [Brief description of
-        responsibilities and achievements] [Job Title],
-        [Company/Organization], [Dates] [Brief description of responsibilities
-        and achievements] [Freelance/Contract Work], [Client/Organization],
-        [Dates] [Brief description of projects and contributions]
-      </span>
-      <br />
-      <br /> 
-      <h1 className="text-green-600 font-semibold text-xl">
-        Achievements & Awards
-      </h1>
-      <span>
-        [Award/Recognition], [Organization/Institution], [Year] [Achievement],
-        [Organization/Platform], [Year]
-      </span>
-      <br />
-      <br /> */}
-      <h1 className="text-green-600 font-semibold text-xl"> 
-        Mission Statement
-      </h1>
-      <p>
-        My mission is to leverage my skills and creativity to deliver
-        innovative solutions that exceed client expectations and
-        contribute positively to the digital landscape. I am committed to
-        continuous learning and growth, always seeking new challenges and
-        opportunities to expand my horizons.
-      </p>
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5 text-center">About</h1>
+        <p className="text-justify leading-relaxed">
+        </p> 
+        <br />
+
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span className="block text-justify leading-relaxed">
+          MCA, Anurag University, 2023 - 2025 <br />
+          BSc Computer Science & Cognitive Systems, Loyola Academy, 2020 - 2023 <br />
+          12th, Bhavan's Sri Aurobindo Junior College, 2018 <br />
+          SSC, Pallavi Progressive High School, 2018 <br />
+        </span>
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span className="block text-justify leading-relaxed">
+          Proficient in: Java, Linux, Amazon EC2
+        </span>
+
+        <br />
+        <br />
+
+        <h1 className="text-green-600 font-semibold text-xl"> 
+          Mission Statement
+        </h1>
+        <p className="text-justify leading-relaxed">
+          My mission is to leverage my skills and creativity to deliver
+          innovative solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
     </div>
-  </div>
   )
 }
 
